801583db752489a807b8f266518839152d4236e5
Gaurav Sai Teja <128503039+gaurav-saiteja@users.noreply.github.com>
2026-08-06 14:52:24 +0530

    Update new2.py

--- new2.py
+++ new2.py
@@ -12,7 +12,7 @@ from datetime import datetime, timedelta
 # --- LOGGING CONFIGURATION ---
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s",
+    format="[%(levelname)s] %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S"
 )
 logger = logging.getLogger(__name__)
