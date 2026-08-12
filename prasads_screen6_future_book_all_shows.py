18f3cb9e4b229e11ddeec2d4552effeb345c027b
Gaurav Sai Teja <128503039+gaurav-saiteja@users.noreply.github.com>
2026-08-12 22:20:24 +0530

    Update prasads_screen6_future_book_all_shows.py

--- prasads_screen6_future_book_all_shows.py
+++ prasads_screen6_future_book_all_shows.py
@@ -21,7 +21,7 @@ logger = logging.getLogger(__name__)
 
 # --- CONFIGURATION ---
 DATES = ["20260813"]
-VENUE_CODE = "ALUC"
+VENUE_CODE = "PRHN"
 STATE_FILE = "sniped_state_11.json"
 MAX_RUNTIME_SECONDS = (5 * 3600) + (55 * 60) # 5 hours 55 mins
 TARGET_TICKET_CATEGORY = "0009" # "0006" for 2D, "0009" for 3D
