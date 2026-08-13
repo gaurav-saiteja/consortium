60fda4216b0f5941846bbd41220b58b4fc2e72ab
Gaurav Sai Teja <128503039+gaurav-saiteja@users.noreply.github.com>
2026-08-13 11:23:44 +0530

    Update allu_cinemas_screen1_future_book_all_shows.py

--- allu_cinemas_screen1_future_book_all_shows.py
+++ allu_cinemas_screen1_future_book_all_shows.py
@@ -51,7 +51,7 @@ PHONE = os.environ.get("BMS_PHONE")
 TOPIC = os.environ.get("NTFY_TOPIC")
 
 DESIRED_SEATS = {
-    "O": ["32", "33", "34"]
+    "R": ["32", "33", "34"]
 }
 
 # Track WARP State natively (Thread-Safe)
